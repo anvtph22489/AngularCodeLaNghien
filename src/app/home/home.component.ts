e03d773e4f5c36fd6179d3eec8dbdce21aacdb71
Van <anvtph22489@fpt.edu.vn>
2023-03-19 03:23:01 +0700

    IF and For basic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'AN';
   public age = 19;
+  public traiCays = ['Cam', 'Đào', 'Nho'];
   constructor() {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('trai cay = ', this.traiCays);
+  }
+  public resetName(): void {
+    console.log('Reset');
+    this.name = '';
+  }
 }

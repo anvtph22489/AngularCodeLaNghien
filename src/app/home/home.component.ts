a6c30b7736c4cbee11c761701660305af0a4b166
Van <anvtph22489@fpt.edu.vn>
2023-03-19 03:38:18 +0700

    Object

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,10 @@ export class HomeComponent implements OnInit {
   public name = 'AN';
   public age = 19;
   public traiCays = ['Cam', 'Đào', 'Nho'];
+  public students = [
+    { name: 'Nguyễn Văn A', age: 19, status: true },
+    { name: 'Trần Đức Bo', age: 11, status: false },
+  ];
   constructor() {}
   ngOnInit(): void {
     console.log('trai cay = ', this.traiCays);

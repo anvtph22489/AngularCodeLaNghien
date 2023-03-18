3068cacccb2ba6c8bab2adb8340d3759c2030849
Van <anvtph22489@fpt.edu.vn>
2023-03-19 02:57:12 +0700

    ngIf

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  public name = 'AN';
+  public age = 19;
+  constructor() {}
+  ngOnInit(): void {}
+}
